64dba1a963b2f1d027cd28642a74737d865e811e
afridayk9 <99346691+afridayk9@users.noreply.github.com>
2024-03-28 20:00:38 -0400

    Add project files.

---
+++ Lab6_LibraryManager/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TableModels;
+
+
+namespace Lab6_LibraryManager;
+
+    /// <summary>
+    /// A class to manage and handle the connection to the database
+    /// </summary>
+public class ApplicationDbContext : DbContext
+{
+public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+//create the tables using the data models we made in the 2nd project    
+public virtual DbSet<Users> Users { get; set; }
+public virtual DbSet<Books> Books { get; set; }
+}
+
---
+++ Lab6_LibraryManager/Migrations/20240328235755_init.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab6_LibraryManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Author = table.Column<string>(type: "TEXT", nullable: false),
+                    ISBN = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
